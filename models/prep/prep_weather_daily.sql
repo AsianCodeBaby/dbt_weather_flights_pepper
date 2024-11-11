df176f736ef7e82ba0659a7bb13bcf43d624f012
AsianCodeBaby <michael.luu@gmx.de>
2024-11-11 12:23:48 +0100

    prep 1 test

---
+++ models/prep/prep_weather_daily.sql
@@ -0,0 +1,27 @@
+WITH daily_data AS (
+    SELECT * 
+    FROM {{ref('staging_weather_daily')}}
+),
+add_features AS (
+    SELECT *
+		, DATE_PART('day', date) AS date_day
+		, DATE_PART('month', date) AS date_month
+		, DATE_PART('year',date) AS date_year
+		, EXTRACT (week from date) AS cw
+		, EXTRACT (month from date) AS month_name
+		, EXTRACT (day from date) AS weekday
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+		, (CASE 
+			WHEN month_name in [12, 1, 2] THEN 'winter'
+			WHEN month_name in [3, 4, 5] THEN 'spring'
+            WHEN month_name in [6, 7, 8]  THEN 'summer'
+            WHEN month_name in [9, 10, 11] THEN 'autumn'
+		END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
